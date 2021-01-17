40945f686f2e9202499fd117e9dda89cbd7a1477
Bobby Jones Jr <jones9682@yahoo.com>
2021-01-16 21:38:24 -0500

    added logic to start returning some stats from selected book

--- SearchString.js
+++ SearchString.js
@@ -52,4 +52,46 @@ function getDocStats(fileContent) {
         }
     }
 
+    //Sort the array
+    let wordList = sortProperties(wordDictionary);
+
+    //Return the top 5 words
+    var top5Words = wordList.slice(0, 6);
+
+    //Return the least 5 words
+    var least5Words = wordList.slice(-6, wordList.length);
+
+    //Write the values to the page
+    ULTemplate(top5Words, document.getElementById("mostUsed"));
+    ULTemplate(least5Words, document.getElementById("leastUsed"));
+
+    docLength.innerText = "Document Length:" + text.length;
+    wordCount.innerText = "Word Count:" + wordArry.length;
+
+}
+
+function ULTemplate(items, element) {
+    let rowTemplete = document.getElementById('template-ul-items');
+    let templateHTML = rowTemplete.innerHTML;
+    let resultsHTML = "";
+
+    for (i = 0; i < items.length - 1; i++) {
+        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i[1]] + "time(s)");
+    }
+
+    element.innerHTML = resultsHTML;
+}
+
+function sortProperties(obj) {
+    //First convert the object to an array
+    let rtnArray = Object.entries(obj);
+
+    //Sort the array
+    rtnArray.sort(function (first, second) {
+        return second[1] - first[1];
+    });
+
+    return rtnArray;
+
+    
 }
