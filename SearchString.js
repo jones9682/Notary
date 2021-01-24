115fecb37d3ce48d3c2bc0aabbe43e2478d21b8d
Bobby Jones Jr <jones9682@yahoo.com>
2021-01-24 07:46:26 -0500

    added logic to filter out words and highlight words when searched

--- SearchString.js
+++ SearchString.js
@@ -42,10 +42,15 @@ function getDocStats(fileContent) {
     
     //Holds multiple key-value pairs
     let wordDictionary = {};
+
+    var uncommonWords = [];
+
+    //Filter out the uncommon words
+    uncommonWords = filterStopWords (wordArray);
     
     //Count every word in the wordArray
-    for( let word in wordArray){
-        let wordValue = wordArray[word];
+    for ( let word in uncommonWords) {
+        let wordValue = uncommonWords[word];
         if (wordDictionary[wordValue] > 0) {
             wordDictionary[wordValue] += 1;
         } else {
@@ -73,12 +78,12 @@ function getDocStats(fileContent) {
 }
 
 function ULTemplate(items, element) {
-    let rowTemplete = document.getElementById('template-ul-items');
-    let templateHTML = rowTemplete.innerHTML;
+    let rowTemplate = document.getElementById('template-ul-items');
+    let templateHTML = rowTemplate.innerHTML;
     let resultsHTML = "";
 
     for (i = 0; i < items.length - 1; i++) {
-        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + "time(s)");
+        resultsHTML += templateHTML.replace('{{val}}', items[i][0] + " : " + items[i][1] + " time(s)");
     }
 
     element.innerHTML = resultsHTML;
@@ -94,6 +99,64 @@ function sortProperties(obj) {
     });
 
     return rtnArray;
+}
 
+//Filter out stop words
+
+function filterStopWords(wordArray) {
+    var commonWords = getStopWords();
+    var commonObj = {};
+    var uncommonArr = [];
+
+    for (let i = 0; i < commonWords.length; i++) {
+        commonObj[commonWords[i].trim()] = true;
+    }
+    
+    for (i = 0; i < wordArray.length; i++) {
+        word = wordArray[i].trim().toLowerCase();
+        if (!commonObj[word]) {
+            uncommonArr.push(word); 
+        }
+    }
+
+    return uncommonArr;
+} 
+
+//A list of stop words we don't want to include in stats
+function getStopWords() {
+    return ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your", "ain't", "aren't", "can't", "could've", "couldn't", "didn't", "doesn't", "don't", "hasn't", "he'd", "he'll", "he's", "how'd", "how'll", "how's", "i'd", "i'll", "i'm", "i've", "isn't", "it's", "might've", "mightn't", "must've", "mustn't", "shan't", "she'd", "she'll", "she's", "should've", "shouldn't", "that'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "wasn't", "we'd", "we'll", "we're", "weren't", "what'd", "what's", "when'd", "when'll", "when's", "where'd", "where'll", "where's", "who'd", "who'll", "who's", "why'd", "why'll", "why's", "won't", "would've", "wouldn't", "you'd", "you'll", "you're", "you've"];
+}
+
+//This will highlight the words that you input in search
+function performMark() {
+
+    //Read thr keyword
+    var keyword = document.getElementById("keyword").value;
+    var display = document.getElementById("fileContent");
+
+    var newContent = "";
+
+    //Find all the currently marked items
+    let spans = document.querySelectorAll('mark');
+
+    for (var i = 0; i < spans.length; i++) {
+        spans[i].outerHTML = spans[i].innerHTML;
+    }
+
+    var re = new RegExp(keyword, "gi");
+    var replaceText = "<mark id='markme'>$&</mark>";
+    var bookContent = display.innerHTML;
+
+    //Add the mark to the book content
+    newContent = bookContent.replace(re, replaceText);
+
+    display.innerHTML = newContent;
+    var count = document.querySelectorAll('mark').length;
+    document.getElementById("searchstat").innerHTML = "found " + count + " matches";
+
+    if (count > 0) {
+        var element = document.getElementById("markme");
+        element.scrollIntoView();
+    };
 
 }
